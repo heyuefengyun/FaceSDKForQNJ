b543144ee257ada879e1db2d10609b6d01ff761b
Jing <841653650@qq.com>
2019-08-01 10:01:16 +0800

    first commit

---
+++ Example/Pods/Target Support Files/FaceSDKForQNJ/FaceSDKForQNJ-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FaceSDKForQNJ : NSObject
+@end
+@implementation PodsDummy_FaceSDKForQNJ
+@end
---
+++ Example/Pods/Target Support Files/Pods-FaceSDKForQNJ_Example/Pods-FaceSDKForQNJ_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FaceSDKForQNJ_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FaceSDKForQNJ_Example
+@end
